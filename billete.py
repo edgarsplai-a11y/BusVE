2850885fdfbb2e70a094ff6f0f88a8e5e6556f82
Victor.valverde <victorvhclases@gmail.com>
2025-09-19 19:47:24 +0200

    testcommit

--- billete.py
+++ billete.py
@@ -1,51 +1,21 @@
-plazas = int(input("Ingrese el número de asientos\n"))
-plazas_vendidas = 0
-plazas_max = int(plazas)
-valor_texto = 0
+from bus import bus
+from cliente import cliente 
 
+valor_texto = 0
 vender = True
 
-def venta_billete(plazas, plazas_vendidas):
-    tickets = int(input())
-    if tickets <= plazas:
-        plazas_vendidas += tickets
-        plazas -= plazas_vendidas
-    else:
-        print("Error")
-    return plazas, plazas_vendidas
-
-def devolucion(plazas, plazas_vendidas):
-    tickets = int(input())
-    if tickets <= plazas_vendidas:
-        plazas_vendidas -= tickets
-        plazas += tickets
-    else:
-       plazas=plazas 
-    return plazas, plazas_vendidas
+class billete:
+    def __init__ (self, cliente:cliente,bus:bus):
+        self.bus = bus
+        self.cliente = cliente
 
 
-print("1.- Venta de billetes.")
-print("2.- Devolución de billetes.")
-print("3.- Estado de la venta.")
-print("0.- Salir.")
+    def venta_billete(self):
+        self.bus.setplazas_max() -= 1
+        self.bus.setplazas_vendidas() += 1   
+        return self.bus.getplazas_max(), self.bus.getplazas_vendidas()
 
-while vender:
-    opcion = input()
-    if opcion == "1":
-        plazas, plazas_vendidas = venta_billete(plazas, plazas_vendidas)
-        print("Venta realizada")
-        print(f"Se vendieron: {plazas_vendidas}")
-    elif opcion == "2":
-        plazas, plazas_vendidas = devolucion(plazas, plazas_vendidas)
-        print("Devolución realizada")
-        print(f"Se devolvieron: {plazas}")
-    elif opcion == "3":
-        print("Estado de la venta")
-        print(f"Total: {plazas_max}")
-        print(f"Libre: {plazas}")
-        print(f"Vendido: {plazas_vendidas}")
-    elif opcion == "0":
-        vender = False
-
-
-    
+    def devolucion(self):
+        self.bus.setplazas_max() += 1
+        self.bus.setplazas_vendidas() -= 1   
+        return self.bus.getplazas_max(), self.bus.getplazas_vendidas()
